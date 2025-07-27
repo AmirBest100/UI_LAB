7d598252afe13eccd09e0faeb70fcf88a55cb52f
Amir_the_Best <amir.is.power.and.best@gmail.com>
2025-07-28 00:07:14 +0330

    Update script.js

--- source2/script.js
+++ source2/script.js
@@ -4,8 +4,6 @@ const txt =document.getElementById("h1")
 
 let a = JSON.parse(localStorage.getItem("save")) || false;
 updateMode(a);
-//set
-// localStorage.setItem("save",JSON.stringify(a))
 
 
 
@@ -28,74 +26,3 @@ function updateMode(mode) {
         txt.innerHTML = "light mode";
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//*************************************************************** */
-// if (!localStorage['save']) {
-//     localStorage.setItem("save")
-//     a = lacalStorage['save']
-//     lacalStorage['save'] = false
-// } else {
-//      a = lacalStorage['save']
-//     if(a === true){
-//         txt.setAttribute("class", "h2")
-//         btn.setAttribute("class", "btn_2")
-//         body.setAttribute("class", "dark")
-//         txt.innerHTML="dark mode"
-//     }
-//     if (a = false) {
-//         txt.setAttribute("class", "h1")
-//         btn.setAttribute("class", "btn_1")
-//         body.setAttribute("class", "light")
-//         txt.innerHTML="light mode"
-//     }
-// }
-// btn.addEventListener("click", click)
-// function click() {
-//     a = !a
-//     lacalStorage['save'] = a
-    
-//     if(a = true){
-//         txt.setAttribute("class", "h2")
-//         btn.setAttribute("class", "btn_2")
-//         body.setAttribute("class", "dark")
-//         txt.innerHTML="dark mode"
-//     }
-//     if (a = false) {
-//         txt.setAttribute("class", "h1")
-//         btn.setAttribute("class", "btn_1")
-//         body.setAttribute("class", "light")
-//         txt.innerHTML="light mode"
-//     }
-// }
